8978cedd949be40c93d5b281eeb080bb98dec8bb
Romua1d Shmidtelson <d-w-b@mail.ru>
2019-11-09 21:23:20 +0300

    Update wp-post-rating.php

--- wp-post-rating.php
+++ wp-post-rating.php
@@ -4,7 +4,7 @@
 Plugin Name: Wp Post Rating
 Plugin URI: http://romua1d.ru/wp_post_rating
 Description: Powerful post rating wordpress plugin.
-Version: 1.0.1
+Version: 1.0.2
 Author: Romua1d
 Author URI: https://romua1d.ru
 Text Domain: wp-post-rating
