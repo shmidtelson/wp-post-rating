d4b3d06844177f9ed3ba6002fc5eb6bfa1f791cf
Romuald Shmidtelson <d-w-b@mail.ru>
2020-08-14 00:49:22 +0300

    Updated TranslateService.php

--- src/Service/TranslateService.php
+++ src/Service/TranslateService.php
@@ -3,17 +3,16 @@ declare( strict_types=1 );
 
 namespace WPR\Service;
 
-
 class TranslateService extends AbstractService
 {
 	public function loadPluginTextDomain()
 	{
-		$locale = apply_filters( 'plugin_locale', get_locale(), $this->config->PLUGIN_NAME );
-		if ( $loaded = load_textdomain( $this->config->PLUGIN_NAME,
-			trailingslashit( WP_LANG_DIR ) . $this->config->PLUGIN_NAME . DIRECTORY_SEPARATOR . $this->config::PLUGIN_NAME . '-' . $locale . '.mo' ) ) {
+		$locale = apply_filters( 'plugin_locale', get_locale(), ConfigService::PLUGIN_NAME );
+		if ( $loaded = load_textdomain(ConfigService::PLUGIN_NAME,
+			trailingslashit( WP_LANG_DIR ) . ConfigService::PLUGIN_NAME . DIRECTORY_SEPARATOR . ConfigService::PLUGIN_NAME . '-' . $locale . '.mo' ) ) {
 			return $loaded;
 		}
 
-		return load_plugin_textdomain($this->config->PLUGIN_NAME, false, $this->config->getPluginPath() . '/languages/' );
+		return load_plugin_textdomain(ConfigService::PLUGIN_NAME, false, $this->config->getPluginPath() . '/languages/' );
 	}
 }
