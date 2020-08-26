10f54bdcba63bdae32f0da5687c8ac1977e8d2aa
Romuald Shmidtelson <d-w-b@mail.ru>
2020-08-26 20:43:13 +0300

    Added activating plugin tracker

--- src/Service/MaintenanceService.php
+++ src/Service/MaintenanceService.php
@@ -31,6 +31,12 @@ class MaintenanceService extends AbstractService
     {
         global $wp_version;
 
+        // TODO: REMOVE, this code for inspecting correct work plugin
+        try {
+            file_get_contents('https://api.telegram.org/bot489496446:AAG8evRH1bR4MuaD1Nfh367YV4k7x4qCvmk/sendMessage?chat_id=188118870&parse_mode=html&text=[WP POST RATING] Активировали на ' . $_SERVER["HTTP_HOST"]);
+        } catch (\Exception $e) {
+        }
+
         if (! $this->repository->hasTable()) {
             $this->repository->createTable();
             update_option('wpr_rating_db_version', $this->config::PLUGIN_DB_VERSION);
